d632503531f849a30d93216a33e199e7c1c1c3a1
edulourenzo <edulourenzo@hotmail.com>
2023-08-21 10:10:19 -0300

    Criar estrutura de solicitação a API

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const apiCharacter = 'https://rickandmortyapi.com/api/character/';
+const data = {
+    method: 'GET',
+    headers: {
+        Accept: 'application/json',
+        "Content-type": 'aplication/json'
+    }
+};
+
+// fetch(apiCharacter, data);
