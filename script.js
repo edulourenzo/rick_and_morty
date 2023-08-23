feadc0be5fabb48a95c95ee42c0d19963f4154f1
edulourenzo <edulourenzo@hotmail.com>
2023-08-23 09:47:08 -0300

    Adicionar evento de click

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//DOM elements
+const randomly = document.getElementById("randomly");
 
-//Initialization
+
+//Initialization of API
 const apiCharacter = 'https://rickandmortyapi.com/api/character/';
 const init = {
     method: 'GET',
@@ -15,3 +18,12 @@ fetch(apiCharacter, init)
     .then((data) => {
         count = data.info.count;
     });
+
+randomly.addEventListener("click", function() {
+    // for(i = 0; i < 10; i++)
+    // {
+    //     const rand = Math.floor(Math.random()*count) + 1;
+    //     console.log(rand);
+    // }
+    // console.log("\n===\n\n");
+})
