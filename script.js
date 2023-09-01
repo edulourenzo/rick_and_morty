9eccd10e95981f9aacbd99a1123a249577da7caf
edulourenzo <edulourenzo@hotmail.com>
2023-09-01 11:12:57 -0300

    Inserir imagem no html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //DOM elements
+const image = document.getElementById("image");
 const divId = document.getElementById("container-id");
 const divName = document.getElementById("container-name");
 const divStatus = document.getElementById("container-status");
@@ -41,50 +42,60 @@ function drawNumber(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
-function getCharacter(number, temp) {
-    fetch(`${apiCharacter}${number}`, init)
-        .then((response) => (response.json()))
-        .then((data) => {
-            console.log(typeof(data));
-            temp = data;
-            console.log(typeof(temp));
-        });
-}
-
 async function main(params) {
+    let response = null;
+    
     //draw character number
     const characterNumber = drawNumber(count);
-    // console.log(`Número sorteado: ${characterNumber}`);
+    console.log(`Número sorteado: ${characterNumber}`);
 
     //get json from API
-    const response =  await fetch(`${apiCharacter}${characterNumber}`, init);
+    response =  await fetch(`${apiCharacter}${characterNumber}`, init);
     const character = await response.json();
     console.log(character);
 
     //get url from response
+    //character.image
+    console.log(`URL da imagem: ${character.image}`);
+    
     //get image from API
+    response = await fetch(character.image, init);
+    const imageBlob = await response.blob();
+    console.log(imageBlob);
+
+    // create object from image
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    console.log(`Objeto da imagem: ${imageObjectURL}`);
     //(future) set animation
+    //{...}
+
+    //insert image in to html
+    image.src = imageObjectURL;
+    //??? URL.revokeObjectURL(imageObjectURL); ???
+
     //insert info in grid layout
+    //{...}
+    
 }
 
 randomly.addEventListener("click", main);
     
 
-    // let description;
-    // let symbol;
-    // let value;
+// let description;
+// let symbol;
+// let value;
 
-    // description = document.createElement("span");
-    // description.appendChild(document.createTextNode("Estado: "));
+// description = document.createElement("span");
+// description.appendChild(document.createTextNode("Estado: "));
 
 
-    // symbol = document.createElement("span");
-    // symbol.appendChild(document.createTextNode("\u271D"));
+// symbol = document.createElement("span");
+// symbol.appendChild(document.createTextNode("\u271D"));
 
-    // value = document.createElement("span");
-    // value.appendChild(document.createTextNode("Morto"));
+// value = document.createElement("span");
+// value.appendChild(document.createTextNode("Morto"));
 
-    // divStatus.appendChild(description);
-    // divStatus.appendChild(symbol);
-    // divStatus.appendChild(value);
-    // console.log(divStatus);
+// divStatus.appendChild(description);
+// divStatus.appendChild(symbol);
+// divStatus.appendChild(value);
+// console.log(divStatus);
